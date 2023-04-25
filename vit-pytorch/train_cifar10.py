6001291305086c142394b5bf0bd5b67a4d7e567b
Boqian <33743759+xiyiyia@users.noreply.github.com>
2023-04-25 16:28:36 +0900

    Update train_cifar10.py

--- vit-pytorch/train_cifar10.py
+++ vit-pytorch/train_cifar10.py
@@ -87,10 +87,10 @@ transform_test = transforms.Compose([
 
 # Prepare dataset
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=1)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=1)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
